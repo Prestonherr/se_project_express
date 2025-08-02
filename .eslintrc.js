70f222b5054a2ea223d059ba797309554edfb269
Preston <prestonherr@gmail.com>
2025-08-01 20:07:04 -0500

    Added eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
   rules: {
     "no-console": ["warn", { allow: ["warn", "error"] }],
     "no-underscore-dangle": ["error", { allow: ["_id"] }],
+    "consistent-return": "off",
   },
 };
--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
-const { PORT = 3001 } = process.env;
 const usersRouter = require("./routes/users");
 const routes = require("./routes");
+
+const app = express();
+const { PORT = 3001 } = process.env;
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
